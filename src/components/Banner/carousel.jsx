489b777ba9d90834c8dff71dcbdffcf7e5ef51e8
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-08-07 14:19:41 +0900

    Design : bannerimg 변경

--- src/components/Banner/carousel.jsx
+++ src/components/Banner/carousel.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Banner1 from "@assets/1.jpg";
-import Banner2 from "@assets/2.jpg";
-import Banner3 from "@assets/3.jpg";
-import Banner4 from "@assets/4.jpg";
-import Banner5 from "@assets/5.jpg";
+import BannerImg from "@assets/banner.png";
 
 const Banner = () => {
   const settings = {
@@ -20,11 +16,11 @@ const Banner = () => {
     centerSlidePercentage: true,
   };
   const banners = [
-    { image: Banner1, text: "함꼐하는 등산라이프" },
-    { image: Banner2, text: "함꼐하는 등산라이프" },
-    { image: Banner3, text: "함꼐하는 등산라이프" },
-    { image: Banner4, text: "함꼐하는 등산라이프" },
-    { image: Banner5, text: "함꼐하는 등산라이프" },
+    { image: BannerImg },
+    { image: BannerImg },
+    { image: BannerImg },
+    { image: BannerImg },
+    { image: BannerImg },
   ];
 
   return (
@@ -32,10 +28,11 @@ const Banner = () => {
       <Carousel {...settings}>
         {banners.map((banner, index) => (
           <div key={index}>
-            <img src={banner.image} alt={`Banner ${index + 1}`} />
-            <p className="text-red-500 font-bold z-50 absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 xl:text-3xl lg:text-3xl sm:text-2xl md:text-2xl">
-              {banner.text}
-            </p>
+            <img
+              className="w-full"
+              src={banner.image}
+              alt={`Banner ${index + 1}`}
+            />
           </div>
         ))}
       </Carousel>
