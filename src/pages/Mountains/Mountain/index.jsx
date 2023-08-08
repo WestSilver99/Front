baf2315736e1ffe573f489041e34d285a09fc255
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-08-08 19:18:28 +0900

    Design : mountains 세부 디테일 수정

--- src/pages/Mountains/Mountain/index.jsx
+++ src/pages/Mountains/Mountain/index.jsx
@@ -5,15 +5,19 @@ const MountainDetail = () => {
   const { id } = useParams();
   const post = dummyData.find((item) => item.id === id);
   return (
-    <div className="my-20 lg:w-2/3 lg:mx-auto xl:w-2/3 xl:mx-auto">
-      <img
-        src={post.image}
-        alt={post.title}
-        className="w-[300px] h-[450px] object-cover mb-2"
-      />
-      <div className="text-left">
-        <p className="text-xl font-bold">{post.title}</p>
-        <p className="text-gray-600">{post.time}</p>
+    <div className="lg:w-2/3 lg:mx-auto xl:w-2/3 xl:mx-auto">
+      <div className="flex flex-col items-center justify-center p-4 mb-10">
+        <div>
+          <p className="text-white inline-block ml-2 absolute -mt-5 z-10 text-lg bg-[#96A68B] p-1 rounded-md">
+            {post.title}
+          </p>
+          <img
+            src={post.image}
+            alt={post.title}
+            className="relative w-[850px] h-[600px] object-cover mb-2 sm:h-[240px] md:h-[240px]"
+          />
+          <p className="text-gray-600">{post.time}</p>
+        </div>
       </div>
     </div>
   );
