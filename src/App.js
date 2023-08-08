e3c818a0c58347ec9210500d4efe97f6af607c65
Seo Eun <seo606101@naver.com>
2023-08-08 14:34:15 +0900

    login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "@components/Layout/layout";
 import Home from "@pages/Home";
 import Login from "@pages/Login";
 import Logout from "@pages/Logout";
+import Auth from "@pages/Login/Auth";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
+          <Route path="/oauth/kakao/callback" element={<Auth />} />
         </Routes>
       </Layout>
     </BrowserRouter>
