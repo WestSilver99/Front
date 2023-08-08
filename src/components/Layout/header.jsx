66ebe675032d5fd46b8407761e6fcc192027f3b2
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-08-08 19:04:12 +0900

    Feat: search hidden 설정

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <nav className=" w-full xl:py-6 xl:px-20 md:px-10 sm:mb-10 ">
       <div className=" max-w-screen-xl ml-3 flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center">
-          <span className=" text-2xl text-[#96A68C] shadow-black  xl:text-[60px] lg:text-[80px] font-semibold whitespace-nowrap ">
+          <span className=" text-3xl text-[#96A68C] shadow-black  xl:text-[60px] lg:text-[80px] font-semibold whitespace-nowrap ">
             SANTA
           </span>
         </Link>
@@ -18,7 +18,7 @@ const Header = () => {
               <BsFillPersonFill size={30} color="green" />
             </button>
           </Link>
-          <SearchBar />
+          <SearchBar smHidden="hidden" />
         </div>
       </div>
     </nav>
