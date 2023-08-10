36a4ee5c80423f73543302f4fde9541307ac9aec
Seo Eun <seo606101@naver.com>
2023-08-10 16:56:14 +0900

    redirect url netlify로 수정

--- src/pages/Login/Auth.jsx
+++ src/pages/Login/Auth.jsx
@@ -4,7 +4,8 @@ import axios from "axios";
 const Auth = () => {
   const code = new URL(window.location.href).searchParams.get("code");
   const REST_API_KEY = "c8be748be694cad38049c4bffb002b6c";
-  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
+  const REDIRECT_URI =
+    "https://benevolent-croquembouche-fd718d.netlify.app/oauth/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return <div>{code}</div>;
