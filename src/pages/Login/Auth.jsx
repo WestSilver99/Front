3b3db0fc70a7078601af635bf2f2df22a2f90d60
Seo Eun <seo606101@naver.com>
2023-08-10 17:33:43 +0900

    api키 env파일에 넣음

--- src/pages/Login/Auth.jsx
+++ src/pages/Login/Auth.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import axios from "axios";
 
 const Auth = () => {
   const code = new URL(window.location.href).searchParams.get("code");
-  const REST_API_KEY = "c8be748be694cad38049c4bffb002b6c";
-  const REDIRECT_URI =
-    "https://benevolent-croquembouche-fd718d.netlify.app/oauth/kakao/callback";
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  // const REDIRECT_URI =
+  //   "https://benevolent-croquembouche-fd718d.netlify.app/oauth/kakao/callback";
+  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
+
+  //const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return <div>{code}</div>;
 };
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,9 +7,10 @@ import locationImg from "../../assets/free-icon-map-point-3119614.png";
 import mountainImg from "../../assets/free-icon-mountains-753760.png";
 
 const Login = () => {
-  const REST_API_KEY = "c8be748be694cad38049c4bffb002b6c";
-  const REDIRECT_URI =
-    "https://benevolent-croquembouche-fd718d.netlify.app/oauth/kakao/callback";
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  //const REDIRECT_URI = "https://benevolent-croquembouche-fd718d.netlify.app/oauth/kakao/callback";
+  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
+
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return (
