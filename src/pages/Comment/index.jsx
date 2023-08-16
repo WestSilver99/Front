69d52a7be129acd6544f904b16c38b8e9b03f098
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-08-16 19:25:34 +0900

    Feat : Comment initial

--- src/pages/Comment/index.jsx
+++ src/pages/Comment/index.jsx
@@ -1,88 +1,131 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsFillPencilFill } from "react-icons/bs";
+import { FaTimes } from "react-icons/fa";
+import axios from "axios";
 
 const WritePostWithDate = () => {
-  const [posts, setPosts] = useState([
-    "이곳은 이용후기가 들어갈 부분입니다. 이곳은 용후기가 들어갈 부분입니다. 이곳은 이용후기가 들어갈 부분입니다.",
-  ]);
-  const [newPost, setNewPost] = useState("");
-  const [selectedDate, setSelectedDate] = useState("");
-  const [userName, setUserName] = useState("");
-  const [isWriting, setIsWriting] = useState(false); // 글쓰기 폼 표시 여부 상태 추가
+  const [showModal, setShowModal] = useState(false);
+  const [username, setUsername] = useState("장광진");
+  const [content, setContent] = useState("");
+  const [boardData, setBoardData] = useState([]);
 
-  const handlePostChange = (event) => {
-    setNewPost(event.target.value);
+  useEffect(() => {
+    fetchBoardData();
+  }, []);
+
+  const fetchBoardData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/board/{id}");
+      setBoardData(response.data);
+    } catch (error) {
+      console.error("Error fetching board data:", error);
+    }
   };
 
-  const handleDateChange = (event) => {
-    setSelectedDate(event.target.value);
-  };
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post(`http://localhost:8080/reply/{id}`, {
+        username,
+        content,
+      });
 
-  const handlePostSubmit = () => {
-    if (newPost.trim() !== "") {
-      const postWithUserAndDate = `${
-        userName ? userName + ": " : ""
-      }${newPost} (${selectedDate})`;
-      setPosts([...posts, postWithUserAndDate]);
-      setNewPost("");
-      setSelectedDate("");
-      setIsWriting(false); // 글 작성 후 폼 숨김
+      // const newReply = response.data;
+
+      setShowModal(false);
+
+      fetchBoardData();
+    } catch (error) {
+      console.error("Error:", error);
     }
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md">
-      <div className="mb-4">
-        {isWriting && (
-          <input
-            type="date"
-            className="w-full mt-2 p-2 border rounded"
-            value={selectedDate}
-            onChange={handleDateChange}
-          />
-        )}
-        {isWriting && (
-          <textarea
-            rows="8"
-            className="w-full h-48 p-2 border rounded"
-            value={newPost}
-            onChange={handlePostChange}
-            placeholder="이용후기를 작성해주세요"
-            style={{ resize: "vertical" }}
-          />
-        )}
-        {isWriting ? (
-          <button
-            className="mt-2 py-1 px-3 bg-[#96A68C] text-white rounded hover:bg-blue-600"
-            onClick={handlePostSubmit}
-          >
-            등록
-          </button>
-        ) : (
-          <button
-            className="mt-2 py-1 px-3  text-[#96A68C]"
-            onClick={() => setIsWriting(true)} // 버튼을 클릭하면 글쓰기 폼 표시
-          >
-            <BsFillPencilFill />
-          </button>
-        )}
-      </div>
+    <div>
       <div>
-        <div className="flex items-center">
-          <div className="flex-1 border-t-2 border-gray-500"></div>
-          <h2 className="w-32 text-lg font-semibold mb-2 text-center bg-[#96A68C] text-white rounded-xl relative">
-            이용후기
-          </h2>
-          <div className="flex-1 border-t-2 border-gray-500"></div>
+        <div className="flex relative justify-center my-10">
+          <p className="text-white inline-block ml-2 z-20 text-[28px] sm:text-xl bg-[#829476] p-1 rounded-2xl">
+            이용 후기
+          </p>
+          <button
+            onClick={() => setShowModal(true)}
+            className="absolute -top-1 right-0"
+          >
+            <BsFillPencilFill color="green" size={24} />
+          </button>
+          <div className="absolute inset-x-0 bottom-6 h-px bg-[#829476] transform -translate-y-1/2">
+            {/* Horizontal line */}
+          </div>
         </div>
+      </div>
 
-        <ul className="list-disc pl-6 list-none">
-          {posts.map((post, index) => (
-            <li key={index} className="mb-2">
-              {post}
-            </li>
-          ))}
-        </ul>
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-40 bg-gray-500 bg-opacity-50">
+          <div className="bg-white p-4 rounded shadow-lg">
+            <div className="flex justify-between flex-row">
+              <h2>후기 작성</h2>
+              <button onClick={() => setShowModal(false)}>
+                <FaTimes color="red" />
+              </button>
+            </div>
+            <form className="flex flex-col">
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="border p-2 mb-2"
+                disabled
+              />
+              <input
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className="border p-2 mb-2 w-[800px] h-[400px]"
+              />
+              <button
+                type="button"
+                onClick={handleRegister}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Register
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {/* Display boardData below the button */}
+      <div className="mt-4 w-[850px]">
+        {boardData.map((item, index) => (
+          <div key={index}>
+            <p className="mb-3 font-bold">{item.content}</p>
+            <p className="text-right text-gray-400 ">{item.username}</p>
+            <hr className="bg-[#829476] my-10" />
+          </div>
+        ))}
+        {/* 이게 위에 boardData 예시본 */}
+        <div className="w-[850px]">
+          <div>
+            <p className="mb-3 font-bold">
+              제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발
+            </p>
+            <p className="text-right text-gray-400">장광진</p>
+            <hr className="bg-[#829476] my-10" />
+          </div>
+          <div>
+            <p className="mb-3 font-bold">
+              제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발
+            </p>
+            <p className="text-right text-gray-400 mb-2">장광진</p>
+            <hr className="bg-[#829476] my-10" />
+          </div>
+          <div>
+            <p className="mb-3 font-bold">
+              제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발제발
+            </p>
+            <p className="text-right text-gray-400 mb-2">장광진</p>
+            <hr className="bg-[#829476] my-10" />
+          </div>
+        </div>
+        {/* 이게 위에 boardData 예시본 */}
       </div>
     </div>
   );
