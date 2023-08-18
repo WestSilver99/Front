ea7c07a1d6b4b12bf6833f2b03107b9fdf7d3f82
Seo Eun <seo606101@naver.com>
2023-08-18 21:47:29 +0900

    feat : 카카오 로그인 거의 다 구현

--- src/pages/Login/Auth.jsx
+++ src/pages/Login/Auth.jsx
@@ -1,46 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+//"proxy": "http://localhost:8080"
 
 const Auth = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
-  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-  const REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
-
-  // const REDIRECT_URI = "https://benevolent-croquembouche-fd718d.netlify.app/oauth/kakao/callback";
-
-  //const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   useEffect(() => {
-    const kakaoLogin = async () => {
-      await axios({
-        method: "GET",
-        // url: `http://localhost:3000/login/oauth2/code/kakao/?code=${code}`,
-        url: `/login/oauth2/code/kakao/?code=${code}`,
-        headers: {
-          "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-          "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
-        },
-      })
-        .then((res) => {
-          //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-          console.log(res);
-          // const token = res.headers.authorization;
-          // window.localStorage.setItem("token", token);
-          //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-          localStorage.setItem("name", res.data.account.kakaoName);
-          //로그인이 성공하면 이동할 페이지
-          navigate("/success");
-        })
-        .catch((err) => {
-          console.log("에러", err);
-          window.alert("로그인에 실패했습니다.");
-        });
-    };
+    //////////////////////////////////////////////////////////////
+    //       백엔드로부터 인가코드 넘기고 jwt 토큰 받기
+    ////////////////////////////////////////////////////////////
+    (async () => {
+      try {
+        const res = await axios
+          // 백엔드 주소 뒤에 인가코드 붙여서 GET 설정
+          // 백엔드는 이 주소를 통해 뒤에 붙여져있는 인가코드를 전달 받게 된다.
+          .get(`http://localhost:8080/login/oauth2/code/kakao/?code=${code}`)
+          // 백엔드 쪽에서 보내준 응답 확인
+          .then((response) => {
+            console.log("응답 확인", response);
+            // 이때,
+            // 백엔드로부터 받아온 헤더값에 저장되어있는 authorization 을 접근해 token 이라는 변수에 저장
+            const token = response.headers.authorization;
+            // 이 토큰은 프론트엔드, 즉 현재 내 서버에 저장시킨다.
+            window.localStorage.setItem("token", token);
+          });
+        console.log(res);
+      } catch (e) {
+        // 에러 발생 시, 에러 응답 출력
+        console.error(e);
+      }
 
-    kakaoLogin();
+      // 위에서 setItem 을 사용하여 내부에 저장시킨 토크을 다시 불러온다.
+      // 이때, 내부 저장소에서 가져온 토큰을 다시 token 이라는 변수에 담는다.
+      const token = window.localStorage.getItem("token");
+      ////////////////////////////////////////////////////////////
+      //               백엔드로 토큰 다시 넘기기
+      ////////////////////////////////////////////////////////////
+      try {
+        const res = await axios
+          // 이때, post가 아닌 get으로 접근한다.
+          // 접근 주소는 백엔드에서 설정한 주소로 한다.
+          .get("http://localhost:8080/login/info", {
+            // 헤더값에는 받아온 토큰을 Authorization과 request 에 담아서 보낸다/
+            headers: {
+              Authorization: token,
+              request: token,
+            },
+          })
+          // 위에서 백엔드가 토큰을 잘받고 처리해서 유저정보를 다시 넘겨준다면, 그 응답을 처리한다.
+          // data 라는 변수에 유저 정보를 저장하고, setItem을 사용해 로컬에 다시 저장한다.
+          .then((data) => {
+            window.localStorage.setItem("profile", data);
+            const name = data.data.nickname;
+            console.log(name);
+            // 만약, 유저정보를 잘 불러왔다면 navigate를 사용해 프론트엔드에서 설정한 마이페이지 경로를 설정해서 이동시킨다.
+            if (data) {
+              navigate("/success", { state: { name } });
+            }
+          });
+      } catch (e) {
+        // 에러 발생 시, 에러 응답 출력
+        console.error(e);
+      }
+    })();
   }, []);
 
   return (
--- src/pages/Login/Success.jsx
+++ src/pages/Login/Success.jsx
@@ -1,5 +1,19 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 const Success = () => {
-  return <div>성공</div>;
+  // useLocation 훅을 사용하여 location 객체를 가져옵니다.
+  const location = useLocation();
+
+  // 전달받은 데이터를 location.state에서 추출합니다.
+  const name = location.state?.name;
+
+  return (
+    <div>
+      <p>{name}님, 환영합니다!</p>
+      {/* 나머지 Success 컴포넌트 내용 */}
+    </div>
+  );
 };
 
 export default Success;
